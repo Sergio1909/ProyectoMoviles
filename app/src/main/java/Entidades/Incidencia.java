51ea624185482b650ca5647b3e1fb5d0fb079cdb
JulioIPv5 <julio.huamani@pucp.edu.pe>
2020-05-27 19:29:21 -0500

    Creacion de entidades

---
+++ app/src/main/java/Entidades/Incidencia.java
@@ -0,0 +1,79 @@
+package Entidades;
+
+import java.sql.Blob;
+
+public class Incidencia {
+
+    private String idIncidencia;
+    private String nombreincidencia;
+    private String descripcion;
+    private String ubicacion;
+    private Blob foto;
+    private String estado;
+    private String comentario;
+    private int Usuario_idUsuario;
+
+    public int getUsuario_idUsuario() {
+        return Usuario_idUsuario;
+    }
+
+    public void setUsuario_idUsuario(int usuario_idUsuario) {
+        Usuario_idUsuario = usuario_idUsuario;
+    }
+
+    public String getIdIncidencia() {
+        return idIncidencia;
+    }
+
+    public void setIdIncidencia(String idIncidencia) {
+        this.idIncidencia = idIncidencia;
+    }
+
+    public String getNombreincidencia() {
+        return nombreincidencia;
+    }
+
+    public void setNombreincidencia(String nombreincidencia) {
+        this.nombreincidencia = nombreincidencia;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public Blob getFoto() {
+        return foto;
+    }
+
+    public void setFoto(Blob foto) {
+        this.foto = foto;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+}
---
+++ app/src/main/java/Entidades/Usuario.java
@@ -0,0 +1,58 @@
+package Entidades;
+
+public class Usuario {
+    private String idUsuario;
+    private String nombre;
+    private String codigo;
+    private String correo;
+    private String password;
+    private int rol;
+
+    public String getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(String idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getRol() {
+        return rol;
+    }
+
+    public void setRol(int rol) {
+        this.rol = rol;
+    }
+}
