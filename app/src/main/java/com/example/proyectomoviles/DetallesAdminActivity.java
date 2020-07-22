4ddf5db89e738fa28fe7c8693bf73547ed081d66
Rodrigo5023 <a20163458@pucp.edu.pe>
2020-07-22 02:08:17 -0500

    Primer avance xd

---
+++ app/src/main/java/com/example/proyectomoviles/DetallesAdminActivity.java
@@ -0,0 +1,13 @@
+package com.example.proyectomoviles;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetallesAdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalles_admin);
+    }
+}
