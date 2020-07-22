fabe9774faeb8cf4ea440cc0062af6a13cacf789
JulioIPv5 <julio.huamani@pucp.edu.pe>
2020-07-22 06:22:18 -0500

    Sincronización con Firebase + error en estructura DTO

---
+++ app/src/main/java/com/example/proyectomoviles/Entidades/IncidenciaNoFuncaDTO.java
@@ -0,0 +1,69 @@
+package com.example.proyectomoviles.Entidades;
+
+public class IncidenciaNoFuncaDTO {
+
+    private String estado;
+    private String nombre;
+    private String descripcion;
+    private String lugar;
+    private String foto;
+    private String autor;
+    private String fecha;
+
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getLugar() {
+        return lugar;
+    }
+
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+}
--- app/src/main/java/com/example/proyectomoviles/MainActivity.java
+++ app/src/main/java/com/example/proyectomoviles/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.proyectomoviles;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.example.proyectomoviles.Entidades.IncidenciaNoFuncaDTO;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+    IncidenciaNoFuncaDTO incidenciaNoFuncaDTO = new IncidenciaNoFuncaDTO();
+
 }
